b3f5227cc9249b50af2ed0c1e9fe73b7770ede13
Дмитрий <mitya.kulik@bk.ru>
2022-09-14 14:51:06 +0300

    Исправил ошибки. Сдела ветвление с 1-да 2-нет. Нужно решить проблему того, что программа закрывается когда в if == да.

--- prog.java
+++ prog.java
@@ -1,25 +1,14 @@
 import java.util.Scanner;
 public class prog {
     public static void main(String[] args) {
-        say(); //1 шаг. Открытие калькулятора
-        yes(); //2 шаг. Открытие условия "да продолжить" "нет отмена"
+        kalkulyalor(); //1 шаг. Открытие калькулятора
     }
 
-    static void yes(){
-        Scanner iScanner = new Scanner(System.in);
-        System.out.println("Введите ДА если хотите продолжить!"); 
-        String b = iScanner.nextLine();
-        switch(b){
-            case "да":
-            while (true) {
-                say();
-                yes();
-            }
-        }
-        iScanner.close();
+    static void yes_no(){
+        kalkulyalor();
     }
 
-    static void  say(){
+    static void  kalkulyalor(){
         Scanner iScanner = new Scanner(System.in);
         System.out.printf("Введите число 1: ");
         int number1 = iScanner.nextInt();
@@ -27,21 +16,62 @@ public class prog {
         int number2 = iScanner.nextInt();
         System.out.print("Введите операцию ");
         String operation = iScanner.next();
+        // iScanner.close(); Выходит ошибка Exception in thread "main" java.lang.IllegalStateException: Scanner closed 
         switch(operation){
             case "+":
             System.out.println(number1+number2);
+            Scanner iScanner1 = new Scanner(System.in);
+            System.out.println("Введите 1 если хотите продолжить!\nВведите 2 если хотите остановить!");
+            Integer message_sum = iScanner.nextInt();
+            if (message_sum == 1) {
+                yes_no();
+            } else if (message_sum == 2) {
+                System.out.println("bay");
+            }
+            iScanner1.close();
                 break;
+
             case "*":
             System.out.println(number1*number2);
+            Scanner iScanner3 = new Scanner(System.in);
+            System.out.println("Введите 1 если хотите продолжить!\nВведите 2 если хотите остановить!");
+            Integer message_mult = iScanner.nextInt();
+            if (message_mult == 1) {
+                yes_no();
+            } else if (message_mult == 2) {
+                System.out.println("bay");
+            }
+            iScanner3.close();
                 break;
+
             case "/":
             System.out.println(number1/number2);
+            Scanner iScanner4 = new Scanner(System.in);
+            System.out.println("Введите 1 если хотите продолжить!\nВведите 2 если хотите остановить!");
+            Integer message_div = iScanner.nextInt();
+            if (message_div == 1) {
+                yes_no();
+            } else if (message_div == 2) {
+                System.out.println("bay");
+            }
+            iScanner4.close();
                 break;
+
             case "-":
             System.out.println(number1-number2);
+            Scanner iScanner5 = new Scanner(System.in);
+            System.out.println("Введите 1 если хотите продолжить!\nВведите 2 если хотите остановить!");
+            Integer message_sub = iScanner.nextInt();
+            if (message_sub == 1) {
+                yes_no();
+            } else if (message_sub == 2) {
+                System.out.println("bay");
+            }
+            iScanner5.close();
                 break;
             
             
         }
+        // iScanner.close(); Выходит ошибка Exception in thread "main" java.lang.IllegalStateException: Scanner closed 
     }
 }
